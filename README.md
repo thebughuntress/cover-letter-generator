87e35468a9500656ce24e77ae111a6b22a4e4b5f
Antonia Frey <antonia.frey@outlook.com>
2025-01-31 16:15:39 +0100

    replace with awesome cv template

--- README.md
+++ README.md
@@ -16,24 +16,17 @@ python main.py
 
 ## Files to Edit
 
-- `coverletter_template/personal-data.tex`: Input your personal details here, such as your name, address, and email address. This file contains placeholders for your data, which will be used to generate the cover letter.
-- `coverletter_template/coverletter.tex`: This is the LaTeX template for the cover letter. You can modify the structure and content of the cover letter in this file. The placeholders in this template will be replaced with your personal data from `personal-data.tex` and the input from the Tkinter form.
+- `templates/coverletter.tex`: This is the LaTeX template for the cover letter.
+- `templates/letter.tex`: You can modify the structure and content of the cover letter in this file. The placeholders in this template will be replaced with the input from the Tkinter form.
+- `templates/personal-data.tex`: Input your personal details here, such as your name, address, and email address. This file contains placeholders for your data, which will be used to generate the cover letter.
 
 ## GUI
+
 ![GUI](images/gui.png)
 
 ## Cover Letter Preview
+
 Once you've generated your cover letter, you can view the result in the pdf folder.
 This gives you an idea of how the generated cover letter looks like: [View Cover Letter PDF](pdf/coverletter.pdf)
 
 <img src="images/coverletter.png" alt="Cover Letter Preview" style="border: 2px solid black; width: 75%;" />
-
-
-
-
-
-
-
-
-
-
--- generate_tex.py
+++ generate_tex.py
@@ -5,15 +5,16 @@ def generate_tex(company, role, url, city, country, color, filename, SW_PLACEHOL
 
     if SW_PLACEHOLDER:
         # use star wars template and placeholders
-        PERSONAL_DATA_FILE = "../coverletter_template/personal-data-sw.tex"
-        COVERLETTER_FILENAME = "coverletter-sw.tex"
+        COVERLETTER_PATH = "./templates/coverletter.tex"
+        PERSONAL_DATA_PATH = "../templates/personal-data-sw.tex"
+        LETTER_PATH = "../templates/letter-sw.tex"
+        JOB_POSTING_PATH = "../templates/job-posting.tex"
     else:
         # use local template and placeholders
-        PERSONAL_DATA_FILE = "../coverletter_template/personal-data.tex"
-        COVERLETTER_FILENAME = "coverletter.tex"
-    
-    # Path to the template file
-    template_path = os.path.join(os.getcwd(), "coverletter_template", COVERLETTER_FILENAME)
+        COVERLETTER_PATH = "./templates/coverletter.tex"
+        PERSONAL_DATA_PATH = "../templates/personal-data.tex"
+        LETTER_PATH = "../templates/letter.tex"
+        JOB_POSTING_PATH = "../templates/job-posting.tex"
 
     # Ensure the 'tex' folder exists
     tex_folder = os.path.join(os.getcwd(), "tex")
@@ -23,24 +24,26 @@ def generate_tex(company, role, url, city, country, color, filename, SW_PLACEHOL
     # Generate a the output file path
     output_file_path = os.path.join(tex_folder, f"{filename}.tex")
 
-    if not os.path.exists(template_path):
-        raise FileNotFoundError(f"Template file not found at: {template_path}")
+    if not os.path.exists(COVERLETTER_PATH):
+        raise FileNotFoundError(f"Template file not found at: {COVERLETTER_PATH}")
 
     # Read the template content
-    with open(template_path, "r") as template_file:
+    with open(COVERLETTER_PATH, "r") as template_file:
         tex_content = template_file.read()
 
     # Replace placeholders in the template
-    tex_content = tex_content.replace("COMPANYX", company)
-    tex_content = tex_content.replace("ROLEX", role)
-    tex_content = tex_content.replace("CITYX", city)
-    tex_content = tex_content.replace("COUNTRYX", country)
-    tex_content = tex_content.replace("URLX", url)
-    tex_content = tex_content.replace("PATHTOFILEX", PERSONAL_DATA_FILE)
+    tex_content = tex_content.replace("THISCOMPANY", company)
+    tex_content = tex_content.replace("THISROLE", role)
+    tex_content = tex_content.replace("THISCITY", city)
+    tex_content = tex_content.replace("THISCOUNTRY", country)
+    tex_content = tex_content.replace("THISURL", url)
+    tex_content = tex_content.replace("PATHTOFILE1", PERSONAL_DATA_PATH)
+    tex_content = tex_content.replace("PATHTOFILE2", JOB_POSTING_PATH)
+    tex_content = tex_content.replace("PATHTOFILE3", LETTER_PATH)
 
     # Replace '#' with an empty string if it exists
     color = color.replace("#", "")
-    tex_content = tex_content.replace("COLORX", color)
+    tex_content = tex_content.replace("THISCOLOR", color)
 
     # Write the modified content to the .tex file
     with open(output_file_path, "w") as tex_file:
